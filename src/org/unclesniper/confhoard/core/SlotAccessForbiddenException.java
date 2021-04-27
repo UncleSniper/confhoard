8d9bd117c5e870c1b68e13ce2bdd52abc028abf4
Simon "UncleSniper" Bausch <simon@bausch-alfdorf.de>
2021-04-28 01:12:20 +0200

    Added SlotAccessForbiddenException.slotAction

--- src/org/unclesniper/confhoard/core/SlotAccessForbiddenException.java
+++ src/org/unclesniper/confhoard/core/SlotAccessForbiddenException.java
@@ -6,13 +6,25 @@ public class SlotAccessForbiddenException extends ConfHoardException {
 
 	private final Slot slot;
 
+	private final SlotAction slotAction;
+
 	public SlotAccessForbiddenException(Slot slot, SlotAction action) {
-		super("No permission to " + action.getActionName() + " slot '" + slot.getKey() + '\'');
+		super("No permission to " + (action == null ? "" : action.getActionName()) + " slot '"
+				+ (slot == null ? "" : slot.getKey()) + '\'');
+		if(slot == null)
+			throw new IllegalArgumentException("Slot cannot be null");
+		if(action == null)
+			throw new IllegalArgumentException("Slot action cannot be null");
 		this.slot = slot;
+		slotAction = action;
 	}
 
 	public Slot getSlot() {
 		return slot;
+	}
+
+	public SlotAction getSlotAction() {
+		return slotAction;
 	}
 
 }
