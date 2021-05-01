6223d7070a504faea5db66188be8c30c8416e7c1
Simon "UncleSniper" Bausch <simon@bausch-alfdorf.de>
2021-05-01 04:26:45 +0200

    Added HexUtils.fromHexString()

--- src/org/unclesniper/confhoard/core/util/HexUtils.java
+++ src/org/unclesniper/confhoard/core/util/HexUtils.java
@@ -18,4 +18,27 @@ public class HexUtils {
 		return String.valueOf(chars);
 	}
 
+	public static byte[] fromHexString(String str) {
+		if(str == null)
+			throw new IllegalArgumentException("Hex digit string cannot be null");
+		int length = str.length();
+		byte[] bytes = new byte[(length + 1) / 2];
+		for(int i = -(length % 2); i < length; i += 2) {
+			char hi = i < 0 ? '0' : str.charAt(i);
+			char lo = str.charAt(i + 1);
+			bytes[(i + 1) / 2] = (byte)((hi << 4) | lo);
+		}
+		return bytes;
+	}
+
+	private static int decodeHexDigit(char c) {
+		if(c >= 'A' && c <= 'F')
+			return c - 'A' + 10;
+		if(c >= 'a' && c <= 'f')
+			return c - 'a' + 10;
+		if(c >= '0' && c <= '9')
+			return c - '0';
+		throw new IllegalArgumentException("Invalid hex digit: " + c);
+	}
+
 }
